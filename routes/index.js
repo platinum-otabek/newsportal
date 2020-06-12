4a3f65b92b319fa7c2878534829b6be916574b48
otabek <otabek@localhost.localdomain>
2020-06-12 17:33:53 +0500

    allfilesto pug

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next)=> {
+  res.render('index', { title: 'Bosh sahifa' });
+});
+//about
+router.get('/about', (req, res, next)=> {
+  res.render('about', { title: 'Biz haqimizda' });
+});
+//categori
+router.get('/categori', (req, res, next)=> {
+  res.render('categori', { title: 'Bo`limlar' });
+});
+//latest_news
+router.get('/latest_news', (req, res, next)=> {
+  res.render('latest_news', { title: 'Oxirgi yangiliklar' });
+});
+//contact
+router.get('/contact', (req, res, next)=> {
+  res.render('contact', { title: 'Contact' });
 });
 module.exports = router;
